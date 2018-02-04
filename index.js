34386993a8a555e053b23a5c73da9d9cf5e15b9b
Dalia Maarek <daliamaarek@gmail.com>
2018-02-04 03:15:34 +0200

    first commit, need to test and design

---
+++ index.js
@@ -0,0 +1,64 @@
+var apiai = require('apiai');
+const express = require('express')
+var cors = require('cors')
+const serverApp = express()
+var app = apiai("8e8734539f7a4f5b9aca18477baab3ea");
+var bodyParser = require('body-parser');
+serverApp.use(bodyParser.json()); // support json encoded bodies
+serverApp.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+serverApp.use(cors());
+
+serverApp.get('', function (req, res) {
+  console.log("GETTING")
+  var request = app.textRequest('req', {
+      sessionId: '<unique session id>'
+  });
+
+
+  request.on('response', function(response) {
+    // var speech = response.result['fulfillment']['speech']
+    console.log(response.result['fulfillment']['speech']);
+    res.send({message : response.result['fulfillment']['speech']});
+  });
+
+  request.on('error', function(error) {
+    console.log(error);
+    res.send(error);
+  });
+
+  request.end();
+  // res.status(200).send({message: "this is the message"})
+
+})
+
+serverApp.post('/', function (req, res) {
+  console.log("REQ MESSAGE: "+ JSON.stringify(req.body.message));
+
+  var request = app.textRequest(req.body.message, {
+      sessionId: '<unique session id>'
+  });
+
+
+  request.on('response', function(response) {
+    // var speech = response.result['fulfillment']['speech']
+    console.log(response.result['fulfillment']['speech']);
+    res.send({message: response.result['fulfillment']['speech']});
+  });
+
+  request.on('error', function(error) {
+    console.log("ERROR: " + error);
+    res.send(error);
+  });
+
+  request.end();
+
+})
+
+
+serverApp.listen(3000, () => {
+  console.log('App listening on port 3000!');
+})
+
+module.exports = {serverApp}
